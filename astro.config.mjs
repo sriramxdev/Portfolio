a3fc2016442e6b0f6ebdc08059b9439051eb5578
Sri Ram Sharma <193415805+sriramxdev@users.noreply.github.com>
2026-06-22 17:48:38 +0530

    fix(devops): remove unused cloudflare adapter to fix static build compilation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
@@ -9,11 +8,6 @@ import icon from 'astro-icon';
 export default defineConfig({
   // site: '#',
   // base: '/',
-  adapter: cloudflare({
-    imageService: {
-      build: 'compile'
-    }
-  }),
   vite: {
     plugins: [tailwindcss()]
   },
