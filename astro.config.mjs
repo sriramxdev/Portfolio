f163f1cd10201bd93ab82cfe91cb88005f43471d
Sri Ram Sharma <193415805+sriramxdev@users.noreply.github.com>
2026-06-22 17:43:41 +0530

    sys(devops): configure pnpm public hoisting pattern for sharp binaries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
@@ -9,6 +9,11 @@ import icon from 'astro-icon';
 export default defineConfig({
   // site: '#',
   // base: '/',
+  adapter: cloudflare({
+    imageService: {
+      build: 'compile'
+    }
+  }),
   vite: {
     plugins: [tailwindcss()]
   },
