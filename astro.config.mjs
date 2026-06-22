9dfbdfb5840b2eaf03df09a3b083af1c3c2ef020
Sri Ram Sharma <193415805+sriramxdev@users.noreply.github.com>
2026-06-22 18:27:11 +0530

    fix(devops): configure production site url domain pointer in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: '#',
-  // base: '/',
+   site: 'https://sriramxdev.me',
+   base: '/',
   vite: {
     plugins: [tailwindcss()]
   },
